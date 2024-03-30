cb30c45e2a6046a546bd590e16dfabbe949bd28a
Nithin Daniel <nithindaniel2018@gmail.com>
2024-03-30 16:59:44 +0530

    updated with new install and fetch

--- js/app.js
+++ js/app.js
@@ -39,12 +39,6 @@ if ("serviceWorker" in navigator) {
 
 
 
-// if('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register("/serviceWorker.js");
-// };
-
-
-
 
 
   
--- serviceWorker.js
+++ serviceWorker.js
@@ -9,13 +9,13 @@ const assets = [
   "https://images.unsplash.com/photo-1561395663-385e657f72bc?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 ]
 
-// // self.addEventListener("install", installEvent => {
-// //   installEvent.waitUntil(
-// //     caches.open(my-cache).then(cache => {
-// //       cache.addAll(assets)
-// //     })
-// //   )
-// // })
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(my-cache).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
 
 // // // self.addEventListener("fetch", fetchEvent => {
 // // //   fetchEvent.respondWith(
@@ -101,51 +101,51 @@ const assets = [
 //   )
 // })
 
-// self.addEventListener("fetch", function(event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function(response) {
-//       return response || fetch(event.request)
-//     })
-//   )
-// })
+self.addEventListener("fetch", (event)=> {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request)
+    })
+  )
+})
 
 
 
 
 // Async with MDN
 
-const cacheName = "MyCache_1";
-// const precachedResources = ["/", "/app.js", "/style.css"];
+// const cacheName = "MyCache_1";
+// // const precachedResources = ["/", "/app.js", "/style.css"];
 
-async function precache() {
-  const cache = await caches.open(cacheName);
-  return cache.addAll(assets);
-}
+// async function precache() {
+//   const cache = await caches.open(cacheName);
+//   return cache.addAll(assets);
+// }
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(precache());
-});
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(precache());
+// });
 
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  if (cachedResponse) {
-    return cachedResponse;
-  }
-  try {
-    const networkResponse = await fetch(request);
-    if (networkResponse.ok) {
-      const cache = await caches.open("MyCache_1");
-      cache.put(request, networkResponse.clone());
-    }
-    return networkResponse;
-  } catch (error) {
-    return Response.error();
-  }
-}
+// async function cacheFirst(request) {
+//   const cachedResponse = await caches.match(request);
+//   if (cachedResponse) {
+//     return cachedResponse;
+//   }
+//   try {
+//     const networkResponse = await fetch(request);
+//     if (networkResponse.ok) {
+//       const cache = await caches.open("MyCache_1");
+//       cache.put(request, networkResponse.clone());
+//     }
+//     return networkResponse;
+//   } catch (error) {
+//     return Response.error();
+//   }
+// }
 
-self.addEventListener("fetch", (event) => {
-  if (precachedResources.includes(url.pathname)) {
-    event.respondWith(cacheFirst(event.request));
-  }
-});
+// self.addEventListener("fetch", (event) => {
+//   if (precachedResources.includes(url.pathname)) {
+//     event.respondWith(cacheFirst(event.request));
+//   }
+// });
 
