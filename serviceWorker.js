060e5aba4dd5e06b264ad086a4a13b41c30169e9
Nithin Daniel <nithindaniel2018@gmail.com>
2024-03-30 16:06:12 +0530

    done new sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,13 +1,13 @@
 // const staticDevCoffee = "dev-coffee-site-v1"
-// const assets = [
-//   "/",
-//   "/index.html",
-//   "/css/style.css",
-//   "/js/app.js",
-//   "coffe.jpg",
-//   "https://images.unsplash.com/photo-1541167760496-1628856ab772?q=80&w=1637&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-//   "https://images.unsplash.com/photo-1561395663-385e657f72bc?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-// ]
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "coffe.jpg",
+  "https://images.unsplash.com/photo-1541167760496-1628856ab772?q=80&w=1637&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1561395663-385e657f72bc?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+]
 
 // self.addEventListener("install", installEvent => {
 //   installEvent.waitUntil(
@@ -95,7 +95,8 @@
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("sw-cache").then(function(cache) {
-      return cache.addAll(["index.html"])
+      console.log(cache);
+      return cache.addAll(assets)
     })
   )
 })
